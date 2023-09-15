15a9c3a4aecddbd383728d851c121532f38ae200
Igomesxr <131804971+Igomesxr@users.noreply.github.com>
2023-09-15 12:03:00 +0000

    como importar contatos

---
+++ teste/webmail/email/como_importar_contatos.rb
@@ -0,0 +1,29 @@
+require 'nokogiri'
+require 'open-uri'
+
+seed_file = File.new("como_importar_contatos_seed.rb", "w") # Append to existing seed file
+
+begin
+  doc = Nokogiri::HTML(URI.open('https://www.agenciaw3.digital/ajuda/email-corporativo/email/tutorial/como-importar-contatos'))
+
+  title = doc.css('div.title h3.text-center').text.strip
+  date = doc.css('div.text-secondary.text-center').text.strip
+  text_elements = doc.css('div.cb-text.medium-editor-element p')
+  image_links = doc.css('div.cb-figure img')
+
+  seed_file.puts "tutorial = Tutorial.create(title: '#{title}', date: '#{date}')"
+
+  text_elements.each do |text|
+    content = text.text.strip
+    seed_file.puts "TutorialContent.create(tutorial: tutorial, content: '#{content}')"
+  end
+
+  image_links.each do |image|
+    image_url = image['src']
+    seed_file.puts "TutorialImage.create(tutorial: tutorial, image_url: '#{image_url}')"
+  end
+rescue StandardError => e
+  puts "Ocorreu um erro: #{e.message}"
+ensure
+  seed_file.close if seed_file
+end
