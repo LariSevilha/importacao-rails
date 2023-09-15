d8b4e59bf9af3323288cec251a534dc9db0e0d5f
caioLoyer <100858355+CaioLoyer@users.noreply.github.com>
2023-09-15 14:01:40 +0000

    refactor:fix webmail images

--- teste/webmail/email/como_importar_contatos.rb
+++ teste/webmail/email/como_importar_contatos.rb
@@ -1,7 +1,7 @@
 require 'nokogiri'
 require 'open-uri'
 
-seed_file = File.new("como_importar_contatos_seed.rb", "w") # Append to existing seed file
+seed_file = File.new("como_importar_contatos_seed.rb", "w")
 
 begin
   doc = Nokogiri::HTML(URI.open('https://www.agenciaw3.digital/ajuda/email-corporativo/email/tutorial/como-importar-contatos'))
@@ -9,18 +9,19 @@ begin
   title = doc.css('div.title h3.text-center').text.strip
   date = doc.css('div.text-secondary.text-center').text.strip
   text_elements = doc.css('div.cb-text.medium-editor-element p')
-  image_links = doc.css('div.cb-figure img')
 
-  seed_file.puts "tutorial = Tutorial.create(title: '#{title}', date: '#{date}')"
+  seed_file.puts "ComoImportarContatosTutorial.create(title: '#{title}', date: '#{date}')"
 
   text_elements.each do |text|
     content = text.text.strip
-    seed_file.puts "TutorialContent.create(tutorial: tutorial, content: '#{content}')"
+    seed_file.puts "ComoImportarContatosTutorialContent.create(tutorial_id: ComoImportarContatosTutorial.last.id, content: '#{content}')"
   end
 
-  image_links.each do |image|
-    image_url = image['src']
-    seed_file.puts "TutorialImage.create(tutorial: tutorial, image_url: '#{image_url}')"
+  images = doc.css('div.cb-content figure.cb-figure img')
+  image_urls = images.map { |image| image['src'] }
+
+  image_urls.each_with_index do |url, index|
+    seed_file.puts "Image.create(tutorial_id: ComoImportarContatosTutorial.last.id, url: '#{url}', caption: 'Imagem #{index + 1}')"
   end
 rescue StandardError => e
   puts "Ocorreu um erro: #{e.message}"
--- teste/webmail/email/mensagem_de_ferias.rb
+++ teste/webmail/email/mensagem_de_ferias.rb
@@ -1,7 +1,7 @@
 require 'nokogiri'
 require 'open-uri'
 
-seed_file = File.new("mensagem_de_ferias_seed.rb", "w") # Use "a" para adicionar ao arquivo existente
+seed_file = File.new("mensagem_de_ferias_seed.rb", "w")
 
 begin
   doc = Nokogiri::HTML(URI.open('https://www.agenciaw3.digital/ajuda/email-corporativo/email/tutorial/mensagem-de-ferias'))
@@ -9,19 +9,19 @@ begin
   title = doc.css('div.title h3.text-center').text.strip
   date = doc.css('div.text-secondary.text-center').text.strip
   text_elements = doc.css('div.cb-text.medium-editor-element p')
-  image_links = doc.css('div.cb-figure img')
 
-  seed_file.puts "\n# Tutorial: #{title}\n"
-  seed_file.puts "Tutorial.create(title: '#{title}', date: '#{date}')"
+  seed_file.puts "MensagemDeFeriasTutorial.create(title: '#{title}', date: '#{date}')"
 
   text_elements.each do |text|
     content = text.text.strip
-    seed_file.puts "TutorialContent.create(tutorial_id: Tutorial.last.id, content: '#{content}')"
+    seed_file.puts "MensagemDeFeriasTutorialContent.create(tutorial_id: MensagemDeFeriasTutorial.last.id, content: '#{content}')"
   end
 
-  image_links.each_with_index do |image, index|
-    image_url = image['src']
-    seed_file.puts "TutorialImage.create(tutorial_id: Tutorial.last.id, image_url: '#{image_url}', order: #{index + 1})"
+  images = doc.css('div.cb-content figure.cb-figure img')
+  image_urls = images.map { |image| image['src'] }
+
+  image_urls.each_with_index do |url, index|
+    seed_file.puts "Image.create(tutorial_id: MensagemDeFeriasTutorial.last.id, url: '#{url}', caption: 'Imagem #{index + 1}')"
   end
 rescue StandardError => e
   puts "Ocorreu um erro: #{e.message}"
